e8aa63112d0b6fcf73b1572db342ff3dfd6c1c8c
Tropical <42101043+Tropix126@users.noreply.github.com>
2024-04-20 23:27:27 -0500

    fix: FreeRTOS-compatible libxil

--- packages/kernel/src/xil/timer.rs
+++ packages/kernel/src/xil/timer.rs
@@ -19,6 +19,8 @@ pub const XPAR_SCUTIMER_INTR: u32 = 29;
 pub struct XScuTimer_Config {
     pub DeviceId: u16,
     pub BaseAddr: *mut c_uint,
+    pub IntrId: u32,
+    pub IntrParent: *mut c_uint,
 }
 
 #[repr(C)]
