95f9e07cd37c41ab4d57199d7583e5a651788c65
doinkythederp <doinkythederp@icloud.com>
2024-08-02 10:22:12 -0700

    refactor: move exit to utils mod

--- kernel/src/drivers/mod.rs
+++ kernel/src/drivers/mod.rs
@@ -1,8 +1,3 @@
 use log::info;
 
 pub mod uart;
-
-pub fn exit(code: i32) -> ! {
-    info!("Exiting with code {}", code);
-    todo!("Exit (code {})", code);
-}
---
+++ kernel/src/utils.rs
@@ -0,0 +1,4 @@
+pub fn exit(code: i32) -> ! {
+    info!("Exiting with code {}", code);
+    todo!("Exit (code {})", code);
+}
