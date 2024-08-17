bf2b87e3c94fc3388756ea910e6a71a0614a6539
Gavin-Niederman <gavinniederman@gmail.com>
2024-08-17 15:02:27 -0700

    fix: update to qemu 9 in flake

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "A very basic flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-24.05";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     rust-overlay.url = "github:oxalica/rust-overlay";
   };
 
