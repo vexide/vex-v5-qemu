ce6781f1d8a74b75c50d36a06fc36dc5141b8a5d
Gavin-Niederman <gavinniederman@gmail.com>
2024-08-19 14:11:35 -0700

    feat: iimproved node minimizing styles

--- host/src/lib/components/NodeBase.svelte
+++ host/src/lib/components/NodeBase.svelte
@@ -3,15 +3,22 @@
     export let barebones = false;
     export let open = true;
 
-    import {ChevronDown} from "svelte-feathers";
+    import { ChevronRight } from "svelte-feathers";
 </script>
 
-<details {open}>
+<details bind:open>
     <summary>
         <header class="node-header">
-            <slot name="handle" />
-            <slot name="icon" />
-            <strong>{title}</strong>
+            <div class="node-header-left">
+                <slot name="handle" />
+                <slot name="icon" />
+                <strong>{title}</strong>
+            </div>
+            <div class="node-header-right">
+                <div class="caret">
+                    <ChevronRight />
+                </div>
+            </div>
         </header>
     </summary>
 
@@ -21,26 +28,20 @@
 </details>
 
 <style>
-    details {
-        background-color: transparent;
-    }
-    details > summary  {
-        display: flex;
-        flex-direction: row-reverse;
-        align-items: center;
-        list-style: none;
-        padding-inline-end: 8px;
+    /* We use a fake version of this because webkit sucks */
+    summary::-webkit-details-marker {
+        display: none;
     }
 
     .node-header {
         position: relative;
         display: flex;
+        flex-direction: row;
         align-items: center;
-        gap: 8px;
+        justify-content: space-between;
         width: 100%;
         height: 36px;
-        padding-inline: 16px;
-        border-radius: 12px 12px 0px 12px;
+        border-radius: 12px;
         background: var(--background-tertiary);
         color: var(--foreground-primary);
         border: none;
@@ -48,6 +49,30 @@
 
     .node-header > :global(svg) {
         color: var(--accent-primary);
+    }
+
+    .node-header-left {
+        display: flex;
+        gap: 8px;
+        padding-inline: 16px;
+    }
+
+    .node-header-right {
+        display: flex;
+        align-items: end;
+        gap: 8px;
+        padding-inline: 4px;
+    }
+
+    details[open] .node-header-right {
+        padding-inline: 8px;
+    }
+
+    .caret {
+        transition: transform 0.2s;
+    }
+    details[open] .caret {
+        transform: rotate(90deg);
     }
 
     .node-body {
